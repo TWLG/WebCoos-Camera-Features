18a0803d892199205309d1a9dd6ae6ebc36312b0
TWLG <deathweapon@cloisterschool.com>
2024-03-20 17:03:12 -0400

    .

--- blueprints/latest_image_v1_bp.py
+++ blueprints/latest_image_v1_bp.py
@@ -1,41 +1,70 @@
 
 from flask import Blueprint, request, current_app
 
+from python_scripts.LatestImageV1.LatestImageV1 import run_image_v1
+from utils import socketIO
 
 latest_image_v1_bp = Blueprint('LatestImageV1', __name__)
 
 
 @latest_image_v1_bp.route('/updateInterval', methods=['POST'])
 def update_interval():
-
+    # Get the new interval from the request
     interval = request.form.get('interval')
-    result = current_app.config['Latest_Image_V1'].update_interval(
-        int(interval))
-    current_app.config['Latest_Image_V1'].socketIO.emit('interface_console', {
-        'message': result}, namespace='/')
+    current_app.config['LATEST_IMAGE_V1_INTERVAL'] = interval
 
-    return result
+    from dotenv import load_dotenv, set_key
+
+    load_dotenv()  # load variables from .env
+    set_key('.env', 'LATEST_IMAGE_V1_INTERVAL', str(interval))
+
+    socketIO.emit('interface_console', {
+        'message': 'Interval updated'}, namespace='/')
+
+    return "Interval updated. Restart job to apply changes."
 
 
 @latest_image_v1_bp.route('/start', methods=['POST'])
 def start():
-    result = current_app.config['Latest_Image_V1'].start()
-    current_app.config['Latest_Image_V1'].socketIO.emit('interface_console', {
-        'message': result}, namespace='/')
-    return result
+    scheduler = current_app.config['scheduler']
+
+    job = scheduler.get_job('LatestImageV1')
+    if job is None:
+        scheduler.add_job(run_image_v1, 'interval',
+                          minutes=current_app.config['LATEST_IMAGE_V1_INTERVAL'], id='LatestImageV1')
+
+        socketIO.emit('interface_console', {
+            'message': 'LatestImageV1 job started'}, namespace='/')
+        return "LatestImageV1 job started."
+    else:
+        scheduler.remove_job('LatestImageV1')
+        scheduler.add_job(run_image_v1, 'interval',
+                          minutes=current_app.config['LATEST_IMAGE_V1_INTERVAL'], id='LatestImageV1')
+        socketIO.emit('interface_console', {
+            'message': 'LatestImageV1 job restarted'}, namespace='/')
+        return "LatestImageV1 job restarted."
 
 
 @latest_image_v1_bp.route('/stop', methods=['POST'])
 def stop():
-    result = current_app.config['Latest_Image_V1'].stop()
-    current_app.config['Latest_Image_V1'].socketIO.emit('interface_console', {
-        'message': result}, namespace='/')
-    return result
+    scheduler = current_app.config['scheduler']
+    scheduler.remove_job('LatestImageV1')
+    socketIO.emit('interface_console', {
+        'message': 'LatestImageV1 job stopped'}, namespace='/')
+    return "LatestImageV1 job stopped."
 
 
 @latest_image_v1_bp.route('/status', methods=['POST'])
 def status():
-    result = current_app.config['Latest_Image_V1'].get_running_state()
-    current_app.config['Latest_Image_V1'].socketIO.emit('interface_console', {
-        'message': result}, namespace='/')
-    return result
+    scheduler = current_app.config['scheduler']
+    job = scheduler.get_job('LatestImageV1')
+    if job is None:
+
+        socketIO.emit('interface_console', {
+            'message': 'LatestImageV1 job does not exist'}, namespace='/')
+        return "Job does not exist."
+    else:
+
+        socketIO.emit('interface_console', {
+            'message': 'LatestImageV1 job exists'}, namespace='/')
+        return "Job exists."
